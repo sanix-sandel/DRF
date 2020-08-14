dac48c0ed49e43d9510370e94f2c07905aa904bc
sanix sandel <sanicksikani@gmail.com>
2020-08-14 05:40:03 +0300

    first step

--- DRF/DRF/urls.py
+++ DRF/DRF/urls.py
@@ -1,21 +1,29 @@
-"""DRF URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers 
+from quickstart import views
+
+
+router=routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='restframework'))
 ]
+
+"""
+Because we're using viewsets instead of views, 
+we can automatically generate the URL conf for our API, 
+by simply registering the viewsets with a router class.
+
+Again, if we need more control over the API URLs we can 
+simply drop down to using regular class-based views, and 
+writing the URL conf explicitly.
+
+Finally, we're including default login and logout views 
+for use with the browsable API. That's optional, but useful 
+if your API requires authentication and you want to use the browsable API.
+
+"""
--- DRF/quickstart/serializers.py
+++ DRF/quickstart/serializers.py
@@ -7,7 +7,7 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         model=User
         fields=['url', 'username', 'email','groups']
 
-class GroupSerializer(serializers.HyperlinkedModelSerialzer):
+class GroupSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model=Group
         fields=['url', 'name']        
