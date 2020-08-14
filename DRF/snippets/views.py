00f70c204a3ccfb7776b9ac46945478b695dcec4
sanix sandel <sanicksikani@gmail.com>
2020-08-14 15:56:55 +0300

    Class Based Views

--- DRF/snippets/views.py
+++ DRF/snippets/views.py
@@ -1,49 +1,45 @@
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 
-@api_view(['GET', 'POST'])
-def snippet_list(request):
-    if request.method=='GET':
+
+class SnippetList(APIView):
+    def get(self, request, format=None):
         snippets=Snippet.objects.all()
         serializer=SnippetSerializer(snippets, many=True)
         return Response(serializer.data)
 
-    elif request.method=='POST':
-        data=JSONParser().parse(request)
-        serializer=SnippetSerializer(data=data)
+    def post(self, request, format=None):
+        serializer=SnippetSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)        
+        return Response(serializer.errors, status=400)              
 
 
-"""
-we want to be able to POST to this view from clients that 
-won't have a CSRF token we need to mark the view as csrf_exempt
-"""
+class SnippetDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Snippet.objects.get(pk=pk)
+        except Snippet.DoesNotExist:
+            raise Http404
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def snippet_detail(request, pk):
-    try:
-        snippet=Snippet.objects.get(pk=pk)
-    except Snippet.DoesNotExist:
-        return Response(status=404)
-
-    if request.method=='GET':
+    def get(self, request, pk, format=None):
+        snippet=self.get_object(pk)    
         serializer=SnippetSerializer(snippet)
         return Response(serializer.data)
 
-    elif request.method=='PUT':
-        
-        serializer=SnippetSerializer(snippet, data=data)
+    def put(self, request, pk, format=None):
+        snippet=self.get_object(pk)
+        serializer=SnippetSerializer(snippet, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=400)
 
-    elif request.method=='DELETE':
+    def delete(self, request, pk, format=None):
+        snippet=self.get_object(pk)
         snippet.delete()
-        return Response(status=204)               
+        return Response(status=204)        
